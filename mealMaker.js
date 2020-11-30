86d69cf8994bd1fa6add99fa0c2a55291b000bd7
Chelsea <chelsea.e.rolland@gmail.com>
2020-11-29 22:24:27 -0600

    completed the meal maker randomizer project

--- mealMaker.js
+++ mealMaker.js
@@ -44,14 +44,14 @@ const menu = {
     },
 
     get courses(){
-        if (typeof this._courses === "object") {
+        //if (typeof this._courses === "object") {
             return {
                 //Below utilizes the getter methods for the appetizers, main, and desserts
                 appetizers: this.appetizers,
                 mains: this.mains,
                 desserts: this.desserts
             }
-        }
+        //}
     },
 
     addDishToCourse(courseName, dishName, dishPrice) {
@@ -72,12 +72,12 @@ const menu = {
         //     this.courses.desserts.push(dish);
         // }
 
-        this.courses[courseName].push(dish);
+        this._courses[courseName].push(dish);
     },
 
     getRandomDishFromCourse: function (courseName){
         //Retrieve array of given courses dishes form the menu's courses object
-        const dishes = this.courses[courseName];
+        const dishes = this._courses[courseName];
         //Generate a random index with the dishes length
         const randomIndex = Math.floor(Math.random() * dishes.length);
         //Return a dish from dishes using the random index
@@ -90,10 +90,22 @@ const menu = {
         const main = this.getRandomDishFromCourse("mains");
         const dessert = this.getRandomDishFromCourse("desserts");
 
-        const totalPrice = appetizer.price + main.price + dessert.price;
+        const totalPrice = (appetizer.price + main.price + dessert.price).toFixed(2);
 
         return `Your meal is ${appetizer.name}, ${main.name}, and ${dessert.name}. The price for this 3-course meal is $${totalPrice}.`
     }
-
 };
 
+//3 Course Meal Inserts
+menu.addDishToCourse("appetizers", "Spinach Dip", 9.78);
+menu.addDishToCourse("mains", "Spinach Alfredo Eggplant Pizza", 15.43);
+menu.addDishToCourse("desserts", "Chocolate Mousse Cake", 13.89);
+
+menu.addDishToCourse("appetizers", "Queso", 7.33);
+menu.addDishToCourse("mains", "Quinoa, Forbidden Rice Vegan Bowl", 17.78);
+menu.addDishToCourse("desserts", "Dream Bars", 11.11);
+
+
+//Testing the Random Course Meal Generator
+let meal = menu.generateRandomMeal();
+console.log(meal);
