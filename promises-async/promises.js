fd4e1af7bc55f8f9027b24ec0a919cdb846119f0
Chelsea Rolland <chelsea.e.rolland@gmail.com>
2021-11-02 08:12:24 -0500

    added and created promise with then with both success and fail statements

--- promises-async/promises.js
+++ promises-async/promises.js
@@ -17,3 +17,24 @@ const orderSunglasses = () => new Promise(myExecutor);
 const orderPromise = orderSunglasses();
 
 console.log(orderPromise);
+
+//======================================================================
+console.log("This is the 1st line of code in app.js");
+
+const usingSTO = () => {
+    console.log("Let's test this out.")
+}
+
+setTimeout(usingSTO, 3000);
+
+console.log("This is the last line of the code in app.js.")
+
+//======================================================================
+
+const { checkInventory } = require('./library.js');
+const order = [["sunglasses", 1], ["bags", 2]];
+
+const handleSuccess = (resolvedValue) => console.log(resolvedValue);
+const handleFailure = (rejecedValue) => console.log(rejecedValue);
+
+checkInventory(order).then(handleSuccess, handleFailure);
